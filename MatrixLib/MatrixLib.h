d93b3f83565db425ecdbd751c2b18dd3448a113d
Hassan Mahmood <14besehmahmood@seecs.edu.pk>
2018-02-11 19:39:57 +0500

    Changed Lib file

--- MatrixLib/MatrixLib.h
+++ MatrixLib/MatrixLib.h
@@ -8,6 +8,8 @@ namespace MatrixLib {
 
 	public ref class Class1
 	{
+		int** MatrixAdd(int **A, int **B, int r, int c);
+		int** MatrixSub(int **A, int **B, int r, int c);
 		// TODO: Add your methods for this class here.
 		int ** MatrixMultiply(int ** A, int r1, int c1, int ** B, int r2, int c2);
 	};
